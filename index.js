508492d8a807e1755ee91f64448c6c2a930f49b4
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-07-24 13:42:20 +0500

    bajarildi

---
+++ index.js
@@ -0,0 +1,16 @@
+// function mergeArrays(arr1, arr2) {
+//     let arr  = new Set([...arr1, ...arr2]);
+//     return Array.from(arr).sort((a, b) => a - b);
+//   }
+// =====================================================
+// function sumMul(n, m) {
+//     if (n <= 0 || m <= 0) {
+//       return "INVALID";
+//     }
+//     let sum = 0;
+//     for (let i = n; i < m; i += n) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
+// ========================================================
