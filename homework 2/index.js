31c42d423498645bad4c57b36ad60d1822c76d40
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-07-26 08:30:18 +0500

    bajarildi

---
+++ homework 2/index.js
@@ -0,0 +1,30 @@
+// 1  Stringni arrayga o'girish
+
+// function stringToArray(string){
+//     return string.split(" ");
+//   }
+// ========================================
+// 2 arraydagi elementlarni + bo'lsa - , - bo'lsa + o'girish
+
+// function invert(array) {
+//     return array.map(n => -n);
+// }
+// =========================================
+// 3  kelayotgan ismlarni birinchisi katta harfda kelishi
+
+// function abbrevName(name){
+//     let [first, last] = name.split(" ");
+//     return first[0].toUpperCase() + "." + last[0].toUpperCase();
+//   }
+// ===================================================
+// 4  kelayotgan stringlarni katta harfda qaytarish
+
+// function makeUpperCase(str) {
+//     return str.toUpperCase();
+//    }
+// =====================================================
+// 5 stringni qoshimcha sozlar qoshib qaytarish
+
+// function greet(name){
+//     return "Hello, " + name + " how are you doing today?";
+//   }
