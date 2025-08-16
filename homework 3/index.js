ceb9764d8f364af730327134fd8c595024f0068a
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-08-16 09:51:54 +0500

    bajarildi

---
+++ homework 3/index.js
@@ -0,0 +1,22 @@
+// function findNeedle(haystack) {
+//   return "found the needle at position " + haystack.indexOf("needle")
+// }
+// console.log(findNeedle(["hay", "junk", "needle", "more junk"]))
+// ====================================================================
+// function fakeBin(str) {
+//   return str
+//     .split('')
+//     .map(d => d < '5' ? '0' : '1')
+//     .join('')
+// }
+// console.log(fakeBin("45385593107843568"))
+// =====================================================
+// function doubleChar(str) {
+//   return str
+//     .split('')
+//     .map(c => c + c)
+//     .join('');
+// }
+// console.log(doubleChar("Hello"))
+// console.log(doubleChar("1234!_ "))
+// ========================================================
