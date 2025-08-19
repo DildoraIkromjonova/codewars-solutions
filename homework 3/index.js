1d8cb926988ec3da43ade36aec51eb77f1949275
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-08-19 11:36:22 +0500

    bajarildi

--- homework 3/index.js
+++ homework 3/index.js
@@ -2,7 +2,7 @@
 //   return "found the needle at position " + haystack.indexOf("needle")
 // }
 // console.log(findNeedle(["hay", "junk", "needle", "more junk"]))
-// ====================================================================
+// // ========================================
 // function fakeBin(str) {
 //   return str
 //     .split('')
@@ -10,7 +10,7 @@
 //     .join('')
 // }
 // console.log(fakeBin("45385593107843568"))
-// =====================================================
+// // // =========================================
 // function doubleChar(str) {
 //   return str
 //     .split('')
@@ -19,4 +19,16 @@
 // }
 // console.log(doubleChar("Hello"))
 // console.log(doubleChar("1234!_ "))
-// ========================================================
+// // ====================================
+
+// let arr = [1, 2, 3, 4,]
+// let result = arr.reduce((summ,curent,index, array)=>{
+//   if (curent % 2 === 0){
+//     summ.juft +=curent
+//   }else{
+//   summ.toq +=curent
+//   }
+//   return summ
+// },{juft:0,toq:0,})
+// console.log(result)
+// ===============================
---
+++ homework 4/index.js
@@ -0,0 +1,45 @@
+// function strCount(str, letter){  
+//   let summ = 0
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i]=== letter) {
+//     summ++
+//   }
+// }
+// return summ
+// }
+// console.log(strCount("Hello", "o"))
+// console.log(strCount("Hello", "l"))
+// console.log(strCount("", "z"))
+// ==================================================
+// function testEven(n) {
+//   return n % 2 ===0
+// }
+// console.log(testEven(3))
+// console.log(testEven(12))
+// ====================================================
+// function solution(a, b){
+//   if (a.length < b.length) {
+//    return a + b + a
+//  } else {
+//    return b + a + b
+//  }
+// }
+// console.log(solution("1", "22"))
+// console.log(solution("22", "1"))
+// =====================================================
+// function divisibleBy(numbers, divisor) {
+//   let arr = []
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % divisor === 0) {
+//       arr.push(numbers[i])
+//     }
+//   }
+//   return arr
+// }
+// console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2))
+// =================================================
+// function nameShuffler(str) {
+//   return str.split(" ").reverse().join(" ")
+// }
+// console.log(nameShuffler("john McClane"))
+// ==========================================
