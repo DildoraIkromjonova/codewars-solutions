40b9552938f752664cb15b87c03895b2b6a4675d
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-09-19 22:12:50 +0500

    bajarildi

---
+++ homework 6/index.js
@@ -0,0 +1,28 @@
+// function isIsogram(str){
+//   return new Set(str.toLowerCase()).size === str.length
+
+//   }
+// console.log(isIsogram("Dermatoglyphics")) true
+// console.log(isIsogram("aba")) false
+// ===========================================
+// function sumTwoSmallestNumbers(numbers) {  
+//  let [a, b] = numbers.sort((a,b) => a - b)
+//  return a + b
+// }
+// console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))  7
+// console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))  3453455
+// ===================================================
+// let sum = 0
+// function oddOrEven(array) {
+//   for(let i = 0; i < array.length; i++){
+//     {sum += array[i]}
+//     if( sum[i] %2===0){
+//       return "even"
+//     }else{
+//       return "odd"
+//     }
+//   }
+//   }
+//   console.log(oddOrEven([0]))
+//   console.log(oddOrEven([0, 1, 4]))
+// console.log(oddOrEven([0, -1,-5]))
