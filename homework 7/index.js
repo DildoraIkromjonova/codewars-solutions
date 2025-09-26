1359617318b4cac61b08c1e8dd0a89d6ea16ce14
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-09-26 21:47:10 +0500

    changed

---
+++ homework 7/index.js
@@ -0,0 +1,30 @@
+// function hoopCount (n) {
+//   return n >= 10 ? 
+//     'Great, now move on to tricks' : 'Keep at it until you get it'
+// }
+// console.log(hoopCount(11))
+// =========================================
+// function sumMix(x){
+//  return x.reduce((acc,current) => acc + Number(current)
+// ,0)
+// }
+// console.log(sumMix([9,3,"7", "3"]))
+// ========================================================
+// function arrayPlusArray(arr1, arr2) {
+//   return arr1.concat(arr2).reduce((acc,current) => acc + current,0)
+// }
+// console.log(arrayPlusArray([1,2,3], [4,5,6]))
+// ===========================================================
+// function reverseWords(str){
+//   return str.split(" ").reverse().join(" ")
+// }
+// console.log(reverseWords("hello world"))
+// =============================================================
+// function twoSort(s) {
+//  for(let i = 0; i < s.length; i++){
+//    s.sort()
+//  }
+//   return s[0].split("").join("***")
+// }
+// console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
+// ===================================================================
