11f1aece8058563bd0e81fb151940ea42d44d8be
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-10-03 15:09:36 +0500

    O'zgarishlar haqida xabar

--- homework 7/index.js
+++ homework 7/index.js
@@ -14,7 +14,7 @@
 //   return arr1.concat(arr2).reduce((acc,current) => acc + current,0)
 // }
 // console.log(arrayPlusArray([1,2,3], [4,5,6]))
-// ===========================================================
+// // ===========================================================
 // function reverseWords(str){
 //   return str.split(" ").reverse().join(" ")
 // }
---
+++ homework 8/index.js
@@ -0,0 +1,63 @@
+// function solution(str) {
+//   return str.split('').reverse().join('');
+// }
+// console.log(solution("world"))
+// ================================================
+// function evenOrOdd(number) {
+//   return number % 2 === 0 ?"Even":"Odd"
+//    }
+//    console.log(evenOrOdd(4))
+// ===================================================
+// function opposite(number) {
+//   for( let i = 0; i <= number; i++){
+//     return -number
+//   }
+// }
+// console.log(opposite(1))
+// =====================================================
+// function removeChar(str){
+//  return str.slice(1, -1)
+// }
+// console.log(removeChar("Salom"))
+// =======================================================
+// Write a function "greet" that returns "hello world!'
+// function greet(){
+//   return "hello world!"
+// }
+// console.log(greet())
+// =========================================================
+// function maps(x){
+//   return x.map(n => n * 2)
+// }
+// console.log(maps([1,2,3]))
+// ========================================================
+// function doubleInteger(i) {
+//    for( let j = 0; j <= i; j++){
+//     return i * 2
+//    }
+// }
+// console.log(doubleInteger(2))
+// ============================================================
+// function paperwork(n, m) {
+//   if ( n < 0 || m < 0){
+//     return 0
+//   } else{
+//     return n * m
+//   }
+// }
+// console.log(paperwork(5,5))
+// console.log(paperwork(-5,5))
+// ===========================================================
+// function simpleMultiplication(number) {
+  // if ( number % 2 === 0){
+  //   return number * 8
+  // } else{
+  //   return number * 9
+  // }
+// }
+// console.log(simpleMultiplication(1))
+// ============================================================
+// function smash (words) {
+//   return words.join(" ")
+// }
+// console.log(smash(["hello", " world", "this", "is", "great"]))
